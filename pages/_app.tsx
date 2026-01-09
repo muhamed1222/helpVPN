d9abbc11852c510db90d157e441a473e3ba4c748
outcastsdev-svg <outcastsdev@gmail.com>
2026-01-09 03:01:15 +0300

    feat: add favicon links and apple touch icon to the application head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+                <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/svg+xml" href="/logotype.svg" />
+            </Head>
             <Component {...pageProps} />
             <Analytics />
         </>
